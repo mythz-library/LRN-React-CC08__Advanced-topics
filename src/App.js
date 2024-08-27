efd42eab79a67ae523fea07776a454666b5bebed
Ashen Jayarathne <ashenjay01@gmail.com>
2024-08-27 08:37:34 +0530

    Updating the context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
+import Login from "./context/Login";
 
 class App extends Component {
   state = {
-    currentUser: { name: "Ashen" },
+    // currentUser: { name: "Ashen" },
+    currentUser: null,
+  };
+
+  handleLoggedIn = (username) => {
+    console.log("Getting the user: " + username);
+    const user = { name: "Ashen" };
+    this.setState({ currentUser: user });
   };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <MoviePage />
+          <Login />
         </div>
       </UserContext.Provider>
     );
